7fe2bb75abccbb4bce6fffcb6f9dbf948ca78729
Скажутин Даниил <2027skazhutin.da@letovo.school>
2026-06-03 00:41:44 +0300

    fix(dashboard): remove inaccessible agentation dependency

--- dashboard/web/src/main.tsx
+++ dashboard/web/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Agentation } from "agentation";
 import "./styles/tokens.css";
 import App from "./App";
 
@@ -9,7 +8,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      {import.meta.env.DEV && <Agentation />}
     </BrowserRouter>
   </React.StrictMode>
 );
